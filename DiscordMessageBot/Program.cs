5b6d48efbf3feddb5504de1b5d54ed2392938b27
Ramazan Eren Karakus <ramazanerenkarakus@gmail.com>
2023-05-21 18:18:48 +0300

    First Commit

---
+++ DiscordMessageBot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace DiscordMessageBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
